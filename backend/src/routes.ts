4c61d4583ad1c0742f734e32322eee473ce950ec
Lucas Medina <lucasmedinarmc@gmail.com>
2024-01-18 20:03:04 -0300

    feat: Created route to list users

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,10 +3,12 @@ import { Router } from 'express'
 // Controllers
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { ListUsersController } from './controllers/user/ListUsersController'
 
 const router = Router()
 
-router.post('/usuarios', new CreateUserController().handle)
+router.post('/usuario', new CreateUserController().handle)
 router.post('/login', new AuthUserController().handle)
+router.get('/usuarios', new ListUsersController().handle)
 
 export { router }
