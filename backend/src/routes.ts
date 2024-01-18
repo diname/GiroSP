d1ba008750e4e2a876ba3d6bbf429c323059efc9
Lucas Medina <lucasmedinarmc@gmail.com>
2024-01-18 20:22:17 -0300

    feat: Created route to get user

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express'
 
+// Middlewares
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
 // Controllers
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { ListUsersController } from './controllers/user/ListUsersController'
+import { GetUserController } from './controllers/user/GetUserController'
 
 const router = Router()
 
-router.post('/usuario', new CreateUserController().handle)
-router.post('/login', new AuthUserController().handle)
-router.get('/usuarios', new ListUsersController().handle)
+router.post('/usuario', isAuthenticated, new CreateUserController().handle)
+router.post('/login', isAuthenticated, new AuthUserController().handle)
+router.get('/usuario', isAuthenticated, new GetUserController().handle)
+router.get('/usuarios', isAuthenticated, new ListUsersController().handle)
 
 export { router }
