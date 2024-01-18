06db4e2ae2915c5cf3a61791c0bb1662e98ecce0
Lucas Medina <lucasmedinarmc@gmail.com>
2024-01-18 19:44:00 -0300

    feat: Created route for user login

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,12 @@
-import { Router, Request, Response } from 'express'
+import { Router } from 'express'
+
+// Controllers
 import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
 
 const router = Router()
 
 router.post('/usuarios', new CreateUserController().handle)
+router.post('/login', new AuthUserController().handle)
 
 export { router }
