fb88fca3322f7eb90a3ccee61c4c73dbd763de98
Lucas Medina <lucasmedinarmc@gmail.com>
2024-03-30 21:00:08 -0300

    remove: delete all migrations

--- prisma/migrations/20240118012848_update_tables/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "propostas" ADD COLUMN     "status" BOOLEAN NOT NULL DEFAULT true;
-
--- AlterTable
-ALTER TABLE "usuarios" ALTER COLUMN "status" SET DEFAULT true;
--- prisma/migrations/20240127161635_create_roles/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "AccessType" AS ENUM ('USER', 'ADMIN', 'VENDEDOR');
-
--- AlterTable
-ALTER TABLE "usuarios" ADD COLUMN     "access" "AccessType" NOT NULL DEFAULT 'USER';
-
--- AlterTable
-ALTER TABLE "vendedores" ADD COLUMN     "access" "AccessType" NOT NULL DEFAULT 'VENDEDOR';
