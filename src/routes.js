fc78bfaa4eb51687aafb265cf556063d5592ff76
Lucas Medina <lucasmedinarmc@gmail.com>
2024-05-06 12:40:01 -0300

    refactor: change name of variable routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { controllerUsuarios } from '../controllers/controllerUsuarios.js'
 import { controllerConsultores } from '../controllers/controllerConsultores.js'
-const router = express.Router()
+const routes = express.Router()
 
-router.get('/', (req, res) => {
+routes.get('/', (req, res) => {
   return res.send('Pagina inicial')
 })
-router.get('/usuarios', new controllerUsuarios().listaUsuarios)
-router.get('/consultores', new controllerConsultores().listaConsultores)
+routes.get('/usuarios', new controllerUsuarios().listaUsuarios)
+routes.get('/consultores', new controllerConsultores().listaConsultores)
 
-export default router
+export default routes
