8c652a6f96d637d9008b952afb1ce6ef10feb8dc
Lucas Medina <lucasmedinarmc@gmail.com>
2024-05-14 19:30:17 -0300

    feat: add route categorias

--- src/controllers/controllerCategoria.js
+++ src/controllers/controllerCategoria.js
@@ -1,5 +1,9 @@
+import { serviceCategoria } from '../services/serviceCategoria.js'
+
 export class controllerCategoria {
   async listaCategorias(req, res) {
-    return res.send('Lista de categorias')
+    const service = new serviceCategoria()
+    let categoria = await service.listaCategorias()
+    return res.send(categoria)
   }
 }
---
+++ src/services/serviceCategoria.js
@@ -0,0 +1,6 @@
+export class serviceCategoria {
+  async listaCategorias(req, res) {
+    const message = `Teste lista de categorias`
+    return message
+  }
+}
