5810f14c78024453767dae0ec09443cd4119d7c1
Lucas Medina <lucasmedinarmc@gmail.com>
2024-05-15 19:30:30 -0300

    fix: change name of variable

--- src/controllers/controllerUsuario.js
+++ src/controllers/controllerUsuario.js
@@ -3,7 +3,7 @@ import { serviceUsuario } from '../services/serviceUsuario.js'
 export class controllerUsuario {
   async usuarios(req, res) {
     const service = new serviceUsuario()
-    let usuario = await service.listaUsuarios()
-    return res.send(usuario)
+    const usuarios = await service.listaUsuarios()
+    return res.send(usuarios)
   }
 }
