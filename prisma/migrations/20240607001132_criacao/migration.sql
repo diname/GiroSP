235f21528d286614705b842dd658ad5e32b28f27
Lucas Medina <lucasmedinarmc@gmail.com>
2024-06-06 21:35:14 -0300

    feat: add a user migration

---
+++ prisma/migrations/20240607001132_criacao/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "usuarios" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "rg" TEXT
+);
