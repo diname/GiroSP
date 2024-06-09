66fa43d66a388233a9557ff4189878df5a31e47c
Lucas Medina <lucasmedinarmc@gmail.com>
2024-06-09 19:25:59 -0300

    feat: add function to create consultor

--- src/services/serviceConsultor.js
+++ src/services/serviceConsultor.js
@@ -1,8 +1,16 @@
+import prisma from '../prisma/prisma.js'
+
 export class serviceConsultor {
   async listaConsultores() {
     return `Teste lista de consultores`
   }
   async adicionaConsultor(nome, email) {
+    const consultor = await prisma.consultor.create({
+      data: {
+        nome: nome,
+        email: email
+      }
+    })
     return `Consultor ${nome} adicionado com sucesso!`
   }
 }
--- src/services/serviceUsuario.js
+++ src/services/serviceUsuario.js
@@ -2,12 +2,12 @@ import prisma from '../prisma/prisma.js'
 
 export class serviceUsuario {
   async listaUsuarios() {
-    const users = prisma.usuario.findMany()
+    const users = await prisma.usuario.findMany()
     return users
   }
 
   async adicionaUsuario(nome, email, senha, cpf, rg) {
-    const user = prisma.usuario.create({
+    const user = await prisma.usuario.create({
       data: {
         nome: nome,
         email: email,
